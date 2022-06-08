41b43766828defa2c0ba0db6d9e69d55f78b6ef9
Farok-Hossain <farokafs7@gmail.comm>
2022-06-08 16:44:34 +0600

    live Website part added

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,9 +1,72 @@
 import React from 'react';
 import './Portfolio.css';
+import IMG1 from '../../images/me.png';
+
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Privacy Firewall App',
+    github:'https://github.com',
+    demo: 'https://dribbble.com/shots/18431867-Privacy-Firewall-App'
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'Privacy Firewall App',
+    github:'https://github.com',
+    demo: 'https://dribbble.com/shots/18431867-Privacy-Firewall-App'
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'Privacy Firewall App',
+    github:'https://github.com',
+    demo: 'https://dribbble.com/shots/18431867-Privacy-Firewall-App'
+  },
+  {
+    id: 4,
+    image: IMG1,
+    title: 'Privacy Firewall App',
+    github:'https://github.com',
+    demo: 'https://dribbble.com/shots/18431867-Privacy-Firewall-App'
+  },
+  {
+    id: 5,
+    image: IMG1,
+    title: 'Privacy Firewall App',
+    github:'https://github.com',
+    demo: 'https://dribbble.com/shots/18431867-Privacy-Firewall-App'
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id,image,title,github,demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={image} alt={title} />
+          </div>
+          <h3>{title}</h3>
+          <div className="portfolio__item-cta">
+            <a href={github} className="btn" target="_blank">Github</a>
+            <a href={demo} className="btn btn-primary" target="_blank">Live Demo</a>
+          </div>
+        </article>
+            )
+          })
+        }
+
+      </div>
+    </section>
   )
 }
 
