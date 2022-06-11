5efbc8510c37417aa4165e0a02537db922317afb
Farok-Hossain <farokafs7@gmail.comm>
2022-06-11 15:18:21 +0600

    all files added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import './Footer.css';
+import {GrFacebook} from 'react-icons/gr';
+import {BsInstagram} from 'react-icons/bs';
+import {BsTwitter} from 'react-icons/bs';
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">EGATOR</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      
+      <div className="footer__socials">
+        <a href="https://facebook.com"><GrFacebook></GrFacebook></a>
+        <a href="https://instagram.com"><BsInstagram></BsInstagram></a>
+        <a href="https://twitter.com"><BsTwitter></BsTwitter></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; EGATOR Tutorials. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,9 +1,65 @@
 import React from 'react';
 import './Testimonials.css';
+import avatar1 from '../../images/me2.jpg';
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: avatar1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum dolore libero quas dignissimos ratione voluptate, cupiditate aliquam asperiores vero dicta.'
+  },
+  {
+    avatar: avatar1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum dolore libero quas dignissimos ratione voluptate, cupiditate aliquam asperiores vero dicta.'
+  },
+  {
+    avatar: avatar1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum dolore libero quas dignissimos ratione voluptate, cupiditate aliquam asperiores vero dicta.'
+  },
+  {
+    avatar: avatar1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum dolore libero quas dignissimos ratione voluptate, cupiditate aliquam asperiores vero dicta.'
+  }
+]
 
 const Testimonials = () => {
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper className="container testimonials__container"
+      // install Swiper modules
+      modules={[ Pagination ]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
+        
+        {
+          data.map(({avatar,name,review}, index) => {
+            return (
+              <SwiperSlide key={index} className="testimonial">
+          <div className="client__avatar">
+            <img src={avatar} />
+          </div>
+          <h5 className="client__name">{name}</h5>
+            <small className="client__review">{review}</small>
+        </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
