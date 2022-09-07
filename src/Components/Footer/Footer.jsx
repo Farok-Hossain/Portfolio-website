214df0912f279b5d8ed36f86ebfb49994a96ecd0
Farok-Hossain <farokafs7@gmail.comm>
2022-09-07 22:39:40 +0600

    emailjs added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,36 +1,57 @@
-import React from 'react';
-import './Footer.css';
-import {GrFacebook} from 'react-icons/gr';
-import {BsInstagram} from 'react-icons/bs';
-import {BsTwitter} from 'react-icons/bs';
-
+import React from "react";
+import "./Footer.css";
+import { GrFacebook } from "react-icons/gr";
+import { BsInstagram } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo">EGATOR</a>
+      <a href="#" className="footer__logo">
+        AFS
+      </a>
 
       <ul className="permalinks">
-        <li><a href="#">Home</a></li>
-        <li><a href="about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
-      
+
       <div className="footer__socials">
-        <a href="https://facebook.com"><GrFacebook></GrFacebook></a>
-        <a href="https://instagram.com"><BsInstagram></BsInstagram></a>
-        <a href="https://twitter.com"><BsTwitter></BsTwitter></a>
+        <a href="https://facebook.com">
+          <GrFacebook></GrFacebook>
+        </a>
+        <a href="https://instagram.com">
+          <BsInstagram></BsInstagram>
+        </a>
+        <a href="https://twitter.com" target="_blank">
+          <BsTwitter></BsTwitter>
+        </a>
       </div>
 
       <div className="footer__copyright">
-        <small>&copy; EGATOR Tutorials. All rights reserved.</small>
+        <small>&copy; AFS Tutorials. All rights reserved.</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,45 +1,34 @@
-import React from 'react';
-import './Portfolio.css';
-import IMG1 from '../../images/me.png';
-
+import React from "react";
+import "./Portfolio.css";
+import IMG1 from "../../images/me.png";
+import doctor from "../../images/doctor.PNG";
 
 const data = [
   {
     id: 1,
-    image: IMG1,
-    title: 'Privacy Firewall App',
-    github:'https://github.com',
-    demo: 'https://dribbble.com/shots/18431867-Privacy-Firewall-App'
+    image: doctor,
+    title: "Privacy Firewall App",
+    server: "https://github.com/Farok-Hossain/doctors-portal-server",
+    client: "https://github.com/Farok-Hossain/doctors-portal-client",
+    demo: "https://doctors-portal-ec125.firebaseapp.com",
   },
   {
     id: 2,
-    image: IMG1,
-    title: 'Privacy Firewall App',
-    github:'https://github.com',
-    demo: 'https://dribbble.com/shots/18431867-Privacy-Firewall-App'
+    image: doctor,
+    title: "Privacy Firewall App",
+    server: "https://github.com/Farok-Hossain/doctors-portal-server",
+    client: "https://github.com/Farok-Hossain/doctors-portal-client",
+    demo: "https://doctors-portal-ec125.firebaseapp.com",
   },
   {
     id: 3,
-    image: IMG1,
-    title: 'Privacy Firewall App',
-    github:'https://github.com',
-    demo: 'https://dribbble.com/shots/18431867-Privacy-Firewall-App'
+    image: doctor,
+    title: "Privacy Firewall App",
+    server: "https://github.com/Farok-Hossain/doctors-portal-server",
+    client: "https://github.com/Farok-Hossain/doctors-portal-client",
+    demo: "https://doctors-portal-ec125.firebaseapp.com",
   },
-  {
-    id: 4,
-    image: IMG1,
-    title: 'Privacy Firewall App',
-    github:'https://github.com',
-    demo: 'https://dribbble.com/shots/18431867-Privacy-Firewall-App'
-  },
-  {
-    id: 5,
-    image: IMG1,
-    title: 'Privacy Firewall App',
-    github:'https://github.com',
-    demo: 'https://dribbble.com/shots/18431867-Privacy-Firewall-App'
-  },
-]
+];
 
 const Portfolio = () => {
   return (
@@ -48,26 +37,30 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {
-          data.map(({id,image,title,github,demo}) => {
-            return (
-              <article key={id} className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={image} alt={title} />
-          </div>
-          <h3>{title}</h3>
-          <div className="portfolio__item-cta">
-            <a href={github} className="btn" target="_blank">Github</a>
-            <a href={demo} className="btn btn-primary" target="_blank">Live Demo</a>
-          </div>
-        </article>
-            )
-          })
-        }
-
+        {data.map(({ id, image, title, server, client, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={server} className="btn" target="_blank">
+                  Server Github Code
+                </a>
+                <a href={client} className="btn" target="_blank">
+                  Client Github Code
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Website
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
